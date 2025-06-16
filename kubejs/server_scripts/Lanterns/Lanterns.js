04004c5c9a4d45c82c50bd446cd4bad4098157fd
PatrioticCross <Ethancoil123@gmail.com>
2025-06-16 12:19:11 -0500

    Dynamic Lights work

---
+++ kubejs/server_scripts/Lanterns/Lanterns.js
@@ -0,0 +1,9 @@
+// This script is property of Catalyst Studios for use in the modpack Create Pack. It is under the All Rights Reserved license.
+//It cannot be used or modified outside of Catalyst Studios without explicit permission from Catalyst Studios.
+
+ServerEvents.tags('item', catalyst => {
+    catalyst.add('curios:belt', '/lantern/')
+})
+
+// This script is property of Catalyst Studios for use in the modpack Create Pack. It is under the All Rights Reserved license.
+//It cannot be used or modified outside of Catalyst Studios without explicit permission from Catalyst Studios.
