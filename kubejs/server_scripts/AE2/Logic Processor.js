d4eff524e379c62c18a1e0734710b27099c0d4b4
PatrioticCross <Ethancoil123@gmail.com>
2025-06-19 16:07:56 -0500

    AE2 to create fixes and changes

--- kubejs/server_scripts/AE2/Logic Processor.js
+++ kubejs/server_scripts/AE2/Logic Processor.js
@@ -16,14 +16,16 @@ ServerEvents.recipes((Catalyst) => {
             [
                 // 'create:brass_ingot' is the input.
                 // the transitional item set by "transitionalItem('create:large_cogwheel')" is the item that will be used during the recipe as the item that the input is using to transition to the output.
-                Catalyst.recipes.createDeploying("create:large_cogwheel", [
-                    "create:large_cogwheel",
-                    "ae2:printed_silicon",
-                ]), // like a normal recipe function, is used as a sequence step in this array. Input and output have the transitional item
-                Catalyst.recipes.createDeploying("create:large_cogwheel", [
-                    "create:large_cogwheel",
+                Catalyst.recipes.createDeploying("createappliedkinetics:incomplete_logic_processor", [
+                    "createappliedkinetics:incomplete_logic_processor",
                     "minecraft:redstone",
-                ]),  
+                ]), // like a normal recipe function, is used as a sequence step in this array. Input and output have the transitional item
+                Catalyst.recipes.createDeploying("createappliedkinetics:incomplete_logic_processor", [
+                    "createappliedkinetics:incomplete_logic_processor",
+                    "ae2:printed_silicon",
+                ]), 
+                Catalyst.recipes.createPressing("createappliedkinetics:incomplete_logic_processor",
+                                                "createappliedkinetics:incomplete_logic_processor")                  
             ]
         )
         .transitionalItem("createappliedkinetics:incomplete_logic_processor")
