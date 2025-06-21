6cdf66180206021632c04102d287158869154133
PatrioticCross <Ethancoil123@gmail.com>
2025-06-21 12:59:00 -0500

    Set up Tachyarite with powah

---
+++ kubejs/server_scripts/Etneral Ores/Tachyarite.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(catalyst => {
+    // .energizing([inputs, ...], output, energy)
+	catalyst.recipes.powah.energizing(['eternalores:gem_necroticarite','eternalores:gem_obsidian_shard','eternalores:gem_peridot','powah:crystal_blazing'], 'eternalores:gem_tachyarite', 20000)
+})
