2f061ce9db829feeb6093508299ce7a8450ba8b1
PatrioticCross <Ethancoil123@gmail.com>
2025-06-21 21:52:32 -0500

    Custom Item stuff

--- kubejs/startup_scripts/Custom items/Custom Items.js
+++ kubejs/startup_scripts/Custom items/Custom Items.js
@@ -2,4 +2,5 @@ StartupEvents.registry('item', catalyst => {
     catalyst.create('diamond_sheet').displayName('Diamond Sheet');
     catalyst.create('netherite_sheet').displayName('Netherite Sheet')
     catalyst.create('ground_amethyst_shards').displayName('Ground Amethyst Shards')
+    catalyst.create('plate_wood').displayName('Wooden Plate')
 })
